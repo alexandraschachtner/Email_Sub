7444e7663c2ef167122efe33ee81b6b252f3087e
Alexandra <alexandra.schachtner@gmail.com>
2019-03-21 11:34:13 -0500

    starting new day

--- app/controllers/unsub_controller.rb
+++ app/controllers/unsub_controller.rb
@@ -1,22 +1,23 @@
 class UnsubController < ApplicationController
 
+ require 'subscriber'
+
 	  def index
-			@subscriber = Subscriber.find(email: subscriber_params[:email])
+	
 	  end
-
-		def delete
-
-			if @subscriber.new_record?
-	      # if subscriber_params[:sub] == 'Yes'
-	         return redirect_to root_path, notice: "It looks like you arent signed up.  Do you wanna be?"
-			  else
-				 return redirect_to root_path, notice: "Looks like your are already here!"
-	    # end
-	   end
-	  end
-
-private
-  def subscriber_params
-		params.require(:subscriber).permit(:email, :sub)
-	end
+#
+# 		def delete
+# 			if @subscriber.new_record?
+# 	      # if subscriber_params[:sub] == 'Yes'
+# 	         return redirect_to root_path, notice: "It looks like you arent signed up.  Do you wanna be?"
+# 			  else
+# 				 return redirect_to root_path, notice: "Looks like your are already here!"
+# 	    # end
+# 	   end
+# 	  end
+#
+# private
+#   def subscriber_params
+# 		params.require(:subscriber).permit(:email)
+# 	end
 end
